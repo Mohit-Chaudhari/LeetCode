f05c256da0c0e1143221934608765fe801c858cd
Mohit Suresh Chaudhari <48171001+Mohit-Chaudhari@users.noreply.github.com>
2024-08-07 00:17:39 +0530

    Time: 12 ms (45.96%) | Memory: 45.8 MB (17.31%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,39 +1,23 @@
 class Solution {
 
-    private HashMap<String, Integer> getHM(String word) {
-
-        HashMap<String, Integer> hm = new HashMap<>();
-
-        for(int i = 0; i < word.length(); i++) {
-
-            String ch = String.valueOf(word.charAt(i));
-            if(hm.containsKey(ch)) {
-                hm.put(ch, hm.get(ch) + 1);
-            } else {
-                hm.put(ch, 1);
-            }
+    private Integer[] getCounts(String word) {
+        int[] frequencies = new int[26];
+        for(char c: word.toCharArray()) {
+            frequencies[c - 'a']++;
         }
 
-        return hm;
-    }
-
-    private List<Integer> getCounts(HashMap<String, Integer> mp) {
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(mp.entrySet());
-        List<Integer> counts = new ArrayList<>();
-
-        Collections.sort(entries, (first, second) -> second.getValue().compareTo(first.getValue()));
-
-        for(Map.Entry<String, Integer> entry: entries) {
-            counts.add(entry.getValue());
+        Integer[] counts = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            counts[i] = frequencies[i];
         }
 
+        Arrays.sort(counts, Collections.reverseOrder());
         return counts;
     }
 
     public int minimumPushes(String word) {
         Integer mul = 1, ans = 0, cnt = 0;
-        HashMap<String, Integer> map = getHM(word);
-        List<Integer> counts = getCounts(map);
+        Integer[] counts = getCounts(word);
 
         for(Integer count: counts) {
             ans = ans + (count * mul);
