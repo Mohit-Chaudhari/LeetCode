c4cbbed8fd0bace273233934e3e9bb9f50fc68aa
Mohit Suresh Chaudhari <48171001+Mohit-Chaudhari@users.noreply.github.com>
2024-09-09 21:00:18 +0530

    Time: 7 ms (87.82%) | Memory: 57.1 MB (20.18%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -17,31 +17,25 @@ class Solution {
 
         while(top <= bottom && left <= right) {
 
-            System.out.println("Left to Right");
             // step 1: Left to Right > top++;
             for(int i = left; i <= right; i++) {
                 if(head != null) {
-                    // System.out.println(head.val);
                     matrix[top][i] = head.val;
                     head = head.next;
                 }
                 else matrix[top][i] = -1;
-                System.out.println("matrix: " + matrix[top][i]);
             } 
             top++;
 
-            System.out.println("Top to Bottom");
             // step 2: top to bottom > right--;
             for(int i = top; i <= bottom; i++) {
                 if(head != null) {
                     matrix[i][right] = head.val;
                     head = head.next;
                 } else matrix[i][right] = -1;
-                System.out.println("matrix: " + matrix[i][right]);
             }
             right--;
 
-            System.out.println("Right to Left");
             // step 3: right to left > bottom --;
             if(top <= bottom) {
                 for(int i = right; i >= left; i--) {
@@ -49,12 +43,10 @@ class Solution {
                         matrix[bottom][i] = head.val;
                         head = head.next;
                     } else matrix[bottom][i] = -1;
-                    System.out.println("matrix: " + matrix[bottom][i]);
                 }
                 bottom--;
             }
 
-            System.out.println("Bottom to Top");
             // step 4: bottom to top > left++;
             if(left <= right) {
                 for(int i = bottom; i >= top; i--) {
@@ -62,7 +54,6 @@ class Solution {
                         matrix[i][left] = head.val;
                         head = head.next;
                     } else matrix[i][left] = -1;
-                    System.out.println("matrix: " + matrix[i][left]);
                 }
                 left++;
             }
