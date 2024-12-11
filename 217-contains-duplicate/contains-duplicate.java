fb08f2539abbb7d77e613ccd901ab2e7b53134bf
Mohit Suresh Chaudhari <48171001+Mohit-Chaudhari@users.noreply.github.com>
2024-12-11 19:35:58 +0530

    Time: 11 ms (72.05%) | Memory: 57.9 MB (53.18%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -4,7 +4,7 @@ class Solution {
 
         for(int num: nums) {
             if(set.contains(num)) return true;
-            else set.add(num);
+            set.add(num);
         }
 
         return false;
